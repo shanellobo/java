900f4f0219034608240847aad2c7ecf995c3d30e
shanellobo <shanelllobo06@gmail.com>
2025-03-06 20:17:45 +0400

    Add files via upload

---
+++ classcircle.java
@@ -0,0 +1,24 @@
+public class Circle {
+    private double radius;
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+    public double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        System.out.println("Radius: " + circle.getRadius());
+        System.out.println("Area: " + circle.calculateArea());
+        System.out.println("Circumference: " + circle.calculateCircumference());
+    }
+}
