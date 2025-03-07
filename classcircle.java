6d06bc07d6950c637d5a79af9bb1bea31fa4638e
shanellobo <shanelllobo06@gmail.com>
2025-03-07 21:52:23 +0400

    Add files via upload

--- classcircle.java
+++ classcircle.java
@@ -1,24 +1,25 @@
+import java.util.Scanner;
 public class Circle {
     private double radius;
+
     public Circle(double radius) {
         this.radius = radius;
     }
-    public double getRadius() {
-        return radius;
-    }
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
-    public double calculateArea() {
+
+    public double getArea() {
         return Math.PI * radius * radius;
     }
-    public double calculateCircumference() {
+
+    public double getCircumference() {
         return 2 * Math.PI * radius;
     }
+
     public static void main(String[] args) {
-        Circle circle = new Circle(5.0);
-        System.out.println("Radius: " + circle.getRadius());
-        System.out.println("Area: " + circle.calculateArea());
-        System.out.println("Circumference: " + circle.calculateCircumference());
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the radius");
+        int radius=sc.nextInt();
+        Circle circle = new Circle(radius);
+        System.out.println("Area: " + circle.getArea());
+        System.out.println("Circumference: " + circle.getCircumference());
     }
 }
