c178b63e75f65862be4c210c4e207e8bcde157cb
shanellobo <shanelllobo06@gmail.com>
2025-03-09 17:46:18 +0400

    Add files via upload

--- compoundintr.java
+++ compoundintr.java
@@ -4,7 +4,7 @@ class Main
     public static void main(String args[]) 
     { 
         double principal = 10000, rate = 10.25, time = 5; 
-        double CI = principal * (Math.pow((1 + rate / 100), time)); 
+        double CI = principle * (Math.pow((1 + rate / 100), time)); 
         System.out.println("Compound Interest is "+ CI); 
     } 
 }
